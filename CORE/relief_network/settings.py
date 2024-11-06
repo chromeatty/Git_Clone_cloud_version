6099fa46cd031f509a608f7014d3d6734e8d34f0
Hamish <hamishzwain@gmail.com>
2024-11-06 01:59:15 +0000

    Set DEBUG to False and update ALLOWED_HOSTS to include 127.0.0.1

--- CORE/relief_network/settings.py
+++ CORE/relief_network/settings.py
@@ -22,9 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rv_3%&(myj9slbg62%_#f(ok&!sd4b@!obx6+b$+z)xrf&zqh%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['54.76.141.46', 'ec2-54-76-141-46.eu-west-1.compute.amazonaws.com', 'localhost']
+DEBUG = False
+ALLOWED_HOSTS = ['54.76.141.46', 'ec2-54-76-141-46.eu-west-1.compute.amazonaws.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
